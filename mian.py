c736e9347d8fbee1bd1d5e97f1dd1e0ac33b21d5
Talal-Hassan-Programmer <Talal-Hassan@outlook.com>
2025-08-08 03:17:18 +0300

    adding load_key() for loading keys from key.key

--- mian.py
+++ mian.py
@@ -2,12 +2,18 @@
 from cryptography.fernet import Fernet
 
 
-def gen_key():
+def gen_key(): # Func for genrating Key
     key = Fernet.generate_key()
     with open("key.key","wb") as f:
         f.write(key)
 
 x = input("what to genrate a new key (yes/no) : ").lower()
-if x == "yes":
-    gen_key()
+if x == "yes": # asking for key generation
+    gen_key() 
 
+
+def load_key():
+    with open("key.key", "rb") as f:
+        return f.read()
+    
+print(load_key()) #Checking if the key is loaded
