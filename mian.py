6cea4a841adbbec84288dabdcc300fdd72b0eced
Talal-Hassan-Programmer <Talal-Hassan@outlook.com>
2026-03-10 00:20:29 +0300

    adding del_pass() to delete passwords and setting it

--- mian.py
+++ mian.py
@@ -37,10 +37,43 @@ def get_pass(): # getting all passwords
             name , encrypted = i.strip().split(" : ")
             decrypted = fernet.decrypt(encrypted.encode())
             print(f"{name} : {decrypted.decode()}")
+def del_pass():
+    k = load_key()
+    fernet = Fernet(k)
+    ll = []
+    with open("password.txt", "r") as f:
+        for i in f:
+            name , encrypted = i.strip().split(" : ")
+            decrypted = fernet.decrypt(encrypted.encode())
+            ll.append(f"{name} : {decrypted.decode()}")
+    for i in range(len(ll)):
+        print(i+1,ll[i])
+    pp = int(input("enter the number: "))
+    with open("password.txt", "a") as p:
+    
+        p.write("")
+    if pp <=0:
+        pp = int(input("Please enter the a valid number: "))
+    else:
+        ll.pop(pp-1)
+    
+    with open("password.txt", "w") as p:
+        for i in ll:
+            name, password = i.strip().split(" : ")
 
+            k = load_key()
+
+            fernet = Fernet(k)
+
+            encrypted = fernet.encrypt(password.encode())
+
+            p.write(f"{name} : {encrypted.decode()} \n")
+    
+    
+    pass
 def main():
     while True:
-        ch = ["-add","-view","-generate a new key (for first time do it)","-exit"]
+        ch = ["-add","-delete","-view","-generate a new key (for first time do it)","-exit"]
 
         print("what whould you like to do")
         for i in ch:
@@ -49,6 +82,8 @@ def main():
         user = input().lower()
         if user == "add":
             add_pass()
+        elif user == "delete":
+            del_pass()
         elif user == "view":
             get_pass()
         elif user == "generate a new key":
