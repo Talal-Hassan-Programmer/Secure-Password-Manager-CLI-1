a0b06eebaf12d11b6a3a5f767c948f6d52539a0d
Talal-Hassan-Programmer <Talal-Hassan@outlook.com>
2026-03-10 00:47:44 +0300

    adding guard in del_pass() for password.txt

--- mian.py
+++ mian.py
@@ -44,46 +44,47 @@ def get_pass(): # getting all passwords
                 name , encrypted = i.strip().split(' : ', 1)
                 decrypted = fernet.decrypt(encrypted.encode())
                 print(f"{name} : {decrypted.decode()}")
-    except:
-        print("No passwords")
+    except FileNotFoundError:
+        print("No passwords yet.")
         
 
 
 
 def del_pass(): # password deleting func
-    k = load_key()
-    fernet = Fernet(k)
-    ll = []
-    with open("password.txt", "r") as f:
-        for i in f:
-            name , encrypted = i.strip().split(' : ', 1)
-            decrypted = fernet.decrypt(encrypted.encode())
-            ll.append(f"{name} : {decrypted.decode()}")
-    for i in range(len(ll)):
-        print(i+1,ll[i])
-    pp = int(input("enter the number: "))
-    
-    while True:
-        if pp > 0 and pp <= len(ll):
-            ll.pop(pp-1)
-            break
-        else:
-            pp = int(input("please enter a valid number: "))
-    
-    with open("password.txt", "w") as p:
-        for i in ll:
-            name, password = i.strip().split(' : ', 1)
+    try:
+        k = load_key()
+        fernet = Fernet(k)
+        ll = []
+        with open("password.txt", "r") as f:
+            for i in f:
+                name , encrypted = i.strip().split(' : ', 1)
+                decrypted = fernet.decrypt(encrypted.encode())
+                ll.append(f"{name} : {decrypted.decode()}")
+        for i in range(len(ll)):
+            print(i+1,ll[i])
+        pp = int(input("enter the number: "))
+        
+        while True:
+            if pp > 0 and pp <= len(ll):
+                ll.pop(pp-1)
+                break
+            else:
+                pp = int(input("please enter a valid number: "))
+        
+        with open("password.txt", "w") as p:
+            for i in ll:
+                name, password = i.strip().split(' : ', 1)
 
-            k = load_key()
+                k = load_key()
 
-            fernet = Fernet(k)
+                fernet = Fernet(k)
 
-            encrypted = fernet.encrypt(password.encode())
+                encrypted = fernet.encrypt(password.encode())
 
-            p.write(f"{name} : {encrypted.decode()} \n")
+                p.write(f"{name} : {encrypted.decode()} \n")
     
-    
-    pass
+    except FileNotFoundError:
+        print("No passwords yet.")
 def main():
     while True:
         ch = ["add","delete","view","generate a new key","exit"]
